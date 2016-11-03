3d966f6187e5ee53175bffde7b4b1defc33cdc36
Christophe GERARDIN <cgerardin@elitemail.org>
2016-11-03 10:35:39 +0100

    Simply waiting for a keystroke. Then test the key for reboot or halt the system.

--- src/main.c
+++ src/main.c
@@ -4,10 +4,33 @@
 
 EFI_STATUS
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-
-	InitializeLib(ImageHandle, SystemTable);
-	Print(L"Welcome to KaOS v0.0.1, a world full of carot!\n");
-	for(;;) __asm__("hlt");
+	
+    EFI_INPUT_KEY Key;
+    
+    InitializeLib(ImageHandle, SystemTable);
+	
+	Print(L"\nWelcome to KaOS v0.0.1, a world full of carots !\n\n");
+	Print(L"Press [H] to halt the system, [R] to reboot...\n");
+	
+	while ((SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key)) == EFI_NOT_READY);
+	
+	if(Key.UnicodeChar == 'h') {
+	
+		Print(L"System halted.");
+		for(;;) __asm__("hlt");
+		return EFI_SUCCESS;
+	
+	} else if(Key.UnicodeChar == 'r') {
+	
+		Print(L"Rebooting the system...\n");
+		return EFI_SUCCESS;
+		
+	} else {
+	
+		Print(L"Error : Key pressed is '%c'\n", Key.UnicodeChar);
+		return -1;
+	
+	}
 	
 }
 
