6addc500dabcad7cdbc2f218c7d8c078f3d48a16
Christophe GERARDIN <cgerardin@elitemail.org>
2016-12-15 16:29:24 +0100

    Finally we exit the UEFI land... Arggghhhh =)

--- src/main.c
+++ src/main.c
@@ -6,36 +6,57 @@
 
 EFI_STATUS
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-	
-    EFI_INPUT_KEY Key;
+
+	/*
+	 * UEFI boot loader
+	 */
     
     InitializeLib(ImageHandle, SystemTable);
     
-    uefi_call_wrapper(SystemTable->ConOut->ClearScreen, 1, SystemTable->ConOut); //How to call ClearScreen() without generate warning ?
+    uefi_call_wrapper(SystemTable->ConOut->ClearScreen, 1, SystemTable->ConOut);
 	
 	Print(LOGO);
 	Print(L"\nWelcome to KaOS v0.0.1, the Karrot OS !\n\n");
-	Print(L"Press [H] to halt the system, [R] to reboot...\n");
+	Print(L"Press any key to boot, [H] to halt the system, [R] to reboot...\n");
 	
+	EFI_INPUT_KEY Key;
 	while ((SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key)) == EFI_NOT_READY);
-	
+
 	if(Key.UnicodeChar == 'h') {
-	
+
 		Print(L"System halted.");
 		for(;;) __asm__("hlt");
 		return EFI_SUCCESS;
-	
+
 	} else if(Key.UnicodeChar == 'r') {
-	
+
 		Print(L"Rebooting the system...\n");
 		return EFI_SUCCESS;
-		
-	} else {
-	
-		Print(L"Error : Key pressed is '%c'\n", Key.UnicodeChar);
-		return EFI_UNSUPPORTED;
 	
 	}
 	
+	// Exiting UEFI land
+	Print(L"Booting the kernel...");
+	uefi_call_wrapper(SystemTable->ConOut->ClearScreen, 1, SystemTable->ConOut);
+
+	unsigned long mapSize = 0;
+	unsigned long mapKey;
+	unsigned long descriptorSize;
+	EFI_MEMORY_DESCRIPTOR *memoryMap = NULL;
+	uint32_t descriptorVersion;
+
+	uefi_call_wrapper(SystemTable->BootServices->GetMemoryMap, 5, &mapSize, memoryMap, NULL, &descriptorSize, NULL);
+	mapSize += 2 * descriptorSize;
+
+	uefi_call_wrapper(SystemTable->BootServices->AllocatePool, 3, EfiLoaderData, mapSize, (void **)&memoryMap);
+	uefi_call_wrapper(SystemTable->BootServices->GetMemoryMap, 5, &mapSize, memoryMap, &mapKey, &descriptorSize, &descriptorVersion);
+	uefi_call_wrapper(SystemTable->BootServices->ExitBootServices, 2, ImageHandle, mapKey);
+
+	/*
+	 * Kernel main
+	 */
+
+	while(1);
+
 }
 
