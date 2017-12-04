f5ef8a81521b219218d176412dbd7df54dc3f159
Christophe GERARDIN <cgerardin@elitemail.org>
2017-12-04 22:32:44 +0100

    Adding drawLine function protocol

--- src/drivers/display.h
+++ src/drivers/display.h
@@ -25,6 +25,7 @@ void blitBufferToScreen(uint64_t fb_base_addr, uint32_t *buffer);
  */
 
 void drawPoint(uint32_t *buffer, uint32_t x, uint32_t y, uint32_t color);
+void drawLine(uint32_t *buffer, uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, uint32_t color);
 void drawRectangle(uint32_t *buffer, uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color);
 void drawFillRectangle(uint32_t *buffer, uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color);
 void drawChar(uint32_t *buffer, uint32_t x, uint32_t y, uint32_t color, wchar_t charcode);
